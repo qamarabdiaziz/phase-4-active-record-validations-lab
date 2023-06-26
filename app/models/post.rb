3caa114ef7a58b40f0217e05be08842b5f82865a
qamarabdiaziz <tushez98@gmail.com>
2023-06-26 10:46:17 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    private
+
+  def clickbait_validator
+    clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+
+    unless clickbait_phrases.any? { |phrase| title&.include?(phrase) }
+      errors.add(:title, "should be clickbait-y")
+    end
+  end
+
+  validate :clickbait_validator
 end
